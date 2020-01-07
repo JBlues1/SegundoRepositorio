d5b40c8cc6f4ad269143af9796ac49a1b6e2bd6d
Julio Hernández <Julio Hernndez@192.168.1.6>
2020-01-06 20:37:26 -0500

    Quinto Commit: Actualizando clase TestRunner.

--- ProyectoBDD/src/runner/TestRunner.java
+++ ProyectoBDD/src/runner/TestRunner.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@Cucumber.Options(features = {"features"})
+@Cucumber.Options(features = "features")
 public class TestRunner {
 
 }
